77d3f56ab7727b6023ec7d9c39109652e8950ca6
Rihan44 <asmuela_bustamante@hotmail.es>
2023-10-11 21:14:26 +0200

    copia de seguridad

--- src/App.test.js
+++ src/App.test.js
@@ -5,26 +5,44 @@ import '@testing-library/jest-dom';
 import { StatusParagraph } from './components/Reusables/StatusParagraph';
 
 describe('Comprobamos el background del parrafo', () => {
-  it('el fondo debería ser verde al pasarle check_in', () => {
+
+/*   it('el fondo debería ser verde al pasarle check_in', () => {
+    render(<div>
+      <StatusParagraph status="check_out"> Hola </StatusParagraph>
+    </div>);
+    console.log(screen.getByText("Hola"))
+    expect(screen.getByText("Hola")).toHaveStyle(`background-color: red`);
+  }); */
+
+/*   it('dwdw', () => {
     render(<StatusParagraph status="check_in" />);
     const status = screen.getByTestId("statusParagraph");
+    const style = window.getComputedStyle(status);
 
-    expect(status).toHaveStyle("backgroundColor: #5AD07A");
+    expect(style.rules.backgroundColor).toBe("#5AD07A");
+  }); */
+
+  it('el fondo debería ser rosa al pasarle check_in', () => {
+    render(<StatusParagraph status="check_out" />);
+    const status = screen.getByTestId("statusParagraph");
+
+    expect(status).toHaveStyle("background-color: #5AD07A");
   });
 
   it('el fondo debería ser rosa al pasarle check_out', () => {
     render(<StatusParagraph status="check_out" />);
     const status = screen.getByTestId("statusParagraph");
 
-    expect(status).toHaveStyle("backgroundColor: #FFEDEC");
+    expect(status).toHaveStyle("background-color: #FFEDEC");
   });
 
   it('el fondo debería ser gris al pasarle in_progress', () => {
     render(<StatusParagraph status="in_progress" />);
     const status = screen.getByTestId("statusParagraph");
 
-    expect(status).toHaveStyle("backgroundColor: #E2E2E2");
+    expect(status).toHaveStyle("background-color: #E2E2E2");
   });
+  
 });
 
 
--- src/components/Reusables/StatusParagraph.jsx
+++ src/components/Reusables/StatusParagraph.jsx
@@ -1,46 +1,46 @@
 import styled from "styled-components";
 import React from 'react';
 
-export const StatusParagraph = ({status, children}) => {
+/* export const StatusParagraph = ({ status, children }) => {
 
     const estilos = {
         backgroundColor: status === 'check_in' ? '#5AD07A' : status === 'check_out' ? '#FFEDEC' : status === 'in_progress' ? '#E2E2E2' : '#5AD07A'
     }
 
-    return(
+    return (
         <>
-            <Status style={estilos} $status={status} data-testid='statusParagraph'>
+            <Status  $status={status} data-testid='statusParagraph'>
                 {children}
             </Status>
         </>
     )
-}
+} */
 
-const Status = styled.p`
+export const StatusParagraph = styled.p`
     display: flex;
     align-items: center;
 
-${'' /*     ${(props) => {
-        switch (props.$status) {
+    ${(props) => {
+        switch (props.status) {
             case 'check_in':
                 return `
-                background-color: #5AD07A;
-            `;
+                    background-color: #5AD07A;
+                `;
             case 'check_out':
                 return `
-                background-color: #FFEDEC;
-            `;
+                    background-color: #FFEDEC;
+                `;
             case 'in_progress':
                 return ` 
-                background-color: #E2E2E2;
-            `;
+                    background-color: #E2E2E2;
+                `;
             default:
                 return ` 
-                background-color: #5AD07A;
-            `
+                    background-color: #5AD07A;
+                `
         }
-    }} */}
-
+    }} 
+    
     padding: 15px;
     border-radius: 12px;
 `;
