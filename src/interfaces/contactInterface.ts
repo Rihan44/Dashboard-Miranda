570e2c09e8c87ab8e691057229a4e7a4aa9a88c8
Rihan44 <asmuela_bustamante@hotmail.es>
2023-10-18 16:37:51 +0200

    contactInterface created

---
+++ src/interfaces/contactInterface.ts
@@ -0,0 +1,19 @@
+export interface ContactInterface {
+    id?: string,
+    name: string,
+    email: string,
+    phone: string | number,
+    email_subject: string,
+    email_description: string,
+    date: Date | string, 
+    dateTime: Date | string,
+    isArchived: boolean
+}
+
+export interface ContactInterfaceState {
+    data: ContactInterface[],
+    status: string,
+    statusArchive: string,
+    error: string | null | undefined
+}
+
