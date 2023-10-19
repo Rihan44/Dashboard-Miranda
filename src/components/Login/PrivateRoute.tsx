f0d84db0d7244c07701f24dfe006ef61044bbe55
Rihan44 <asmuela_bustamante@hotmail.es>
2023-10-19 14:49:23 +0200

    PrivateRouter with ts

--- src/components/Login/PrivateRoute.tsx
+++ src/components/Login/PrivateRoute.tsx
@@ -1,9 +1,13 @@
-import { useContext } from "react";
+import React, { useContext } from "react";
 import { Navigate } from "react-router-dom"
 import { AuthContext } from "../Context/AuthContainer";
 
 
-export const PrivateRoute = ({children}) => {
+interface Props {
+    children: React.ReactNode
+}
+
+export const PrivateRoute: React.FC<Props> = ({children}) => {
 
    const { auth } = useContext(AuthContext)
 
